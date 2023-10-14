9ea0fbbd7afefdc9487ee2df722ba3dd516c6bcd
KatSchultz <ks.katieschultz@gmail.com>
2023-10-14 14:28:21 -0400

    nav to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,19 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="header">
-      <h1>My Mad Libs</h1>
+      <Link to="/" className="links">
+        <h1>My Mad Libs</h1>
+      </Link>
       <ul className="header__list">
         <li>
-          <Link to="/stories">Saved Stories</Link>
+          <Link to="/stories" className="links">
+            Saved Stories
+          </Link>
         </li>
         <li>
-          <Link to="/create-story">Create a New Story</Link>
+          <Link to="/create-story" className="links">
+            Create a New Story
+          </Link>
         </li>
       </ul>
     </div>
