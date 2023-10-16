6540b0ea344572e6718754b59bb0c3d1cf103bbd
KatSchultz <ks.katieschultz@gmail.com>
2023-10-15 21:31:05 -0400

    testing configuration

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders saved stories link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/saved stories/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders create story link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/create a new story/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
